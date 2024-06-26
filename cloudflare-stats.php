8219f0c3b21b96994cb9a8bb0a7fda6dae4be0c6
Von Wallace <vonwallace@yahoo.com>
2024-06-26 18:12:15 -0500

    Update cloudflare-stats.php

--- cloudflare-stats.php
+++ cloudflare-stats.php
@@ -59,13 +59,7 @@ echo "<!DOCTYPE html >
             width: 100%;
         }
     </style>
-    <script>
-        (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src=\"https://www.clarity.ms/tag/\"+i+\"?ref=bwt\";
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, \"clarity\", \"script\", \"h7m0khkumk\");
-    </script>
+   
 
     <link href=\"/bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\" crossorigin=\"anonymous\">
     <script src=\"/bootstrap/js/bootstrap.bundle.min.js\"  crossorigin=\"anonymous\"></script>
